6421929a74c875f0586b8bcf89d51e8abe532edd
Jonathan Costa <costajonathan34@gmail.com>
2026-09-14 20:58:01 -0300

    finalizando página e primeiro teste com auditor

--- camara/scripts/diretorio.js
+++ camara/scripts/diretorio.js
@@ -1,3 +1,7 @@
+
+
+// BOTÃO E MENU
+
 const hamBtn = document.querySelector("#ham-btn");
 
 hamBtn.classList.add("botao-hamburguer");
@@ -17,6 +21,35 @@ hamBtn.addEventListener("click", () =>{
     
 })
 
+// MAIN
+
+// FUNÇÃO PARA LER JSON
+
+async function ver_empresas() {
+    const resposta = await fetch("dados/membros.json");
+
+    const empresas = await resposta.json();
+
+    const Lista_de_empresas = document.querySelector("#Lista_de_empresas");
+
+        empresas.forEach((empresa)=>{
+
+            const card = document.createElement("div");
+            card.classList.add("card")
+            card.innerHTML = `
+            <h2>${empresa.Nome}</h2>
+            <img src="${empresa.Imagem}" alt="${empresa.Nome}">
+            <p>${empresa.Endereço}</p>
+            <p>Tel.: ${empresa.Telefone}</p>
+            <p>Site: ${empresa["URL do site"]}</p>
+            <p>Nível de associado: ${empresa["Nível de associação"]}</p>
+            <p>Sobre: ${empresa["Outras informações"]}</p>`;
+
+            Lista_de_empresas.appendChild(card)
+    })
+}
+
+ver_empresas();
 
 
 
