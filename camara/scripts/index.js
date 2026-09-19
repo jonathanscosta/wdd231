03692a9bea9666e5ca291c6e1f5c1c2ae49ecfce
Jonathan Costa <costajonathan34@gmail.com>
2026-09-19 18:03:37 -0300

    ajustes do titulos e dos cards

--- camara/scripts/index.js
+++ camara/scripts/index.js
@@ -20,7 +20,28 @@ hamBtn.addEventListener("click", () => {
 })
 
 // MAIN
+
     
+    const eventos = document.querySelector("#eventos_atuais")
+    const tempo = document.querySelector("#tempo")
+    const empresas_em_destaque = document.querySelector("#empresas_em_destaque")
+    const evento = document.querySelector("#evento")
+    const clima = document.querySelector("#clima")
+
+    evento.innerHTML = `<h2>Eventos</h2>
+    <p>Reunião empresarial</p>
+    <p>25 de Dezembro</p>
+    <p>Camara de Comércio de Arthon</p>`
+
+    // FUNÇÃO PARA CRIAR O CARD DE CLIMA
+    const CHAVE_DE_API = "5628874b7800b7ac335d0cfbb2d7e06b"
+
+    const latitude = 0;
+    const longitude = 0;
+
+    async function buscarClima() {
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`
+    }
 
 
 // FOOTER
